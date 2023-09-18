210067354f10216e03a363e0560dde84262d2e9f
emirgit <emirtr852816@hotmail.com>
2023-09-18 19:32:16 +0300

    GroupAnagram
    GroupAnagram

---
+++ Solutions - Python/Group Anagrams.py
@@ -0,0 +1,26 @@
+class Solution:
+    """
+    Runtime Beats 17.46%
+    Memory  Beats 36.21%
+    """
+    
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        """
+        'a' is 97 in ASCII
+        'z' is 122 in ASCII
+        122 - 97 + 1 = 26 character
+
+        """
+        countlist = {}
+        for i in strs:
+            counter = [0] * (ord('z') - ord('a') + 1 )
+            for char in i:
+                counter[ord(char) - ord('a')] += 1
+            if tuple(counter) in countlist.keys():
+                countlist[tuple(counter)].append(i)
+            else:
+                countlist[tuple(counter)] = [i]
+                
+        return list(countlist.values())
+
+            
