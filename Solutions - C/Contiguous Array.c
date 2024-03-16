44afc996738c88a11d92597d6fb340f90cd65765
emirgit <emirtr852816@hotmail.com>
2024-03-17 01:37:10 +0300

    Create Contiguous Array.c

---
+++ Solutions - C/Contiguous Array.c
@@ -0,0 +1,34 @@
+/*
+Time C.
+66ms ms Beats 78.13%
+Space C.
+7.60 MB Beats 31.25%
+*/
+
+int findMaxLength(int* nums, int numsSize) {
+    int i, res = 0, sum = 0, curCount;
+    int *hash = malloc(sizeof(int) * (numsSize * 2 + 1));
+    for (i = 0; i < 2 * numsSize + 1; i++) {
+        hash[i] = -2;
+    }
+    hash[numsSize] = -1;
+    for(i = 0; i < numsSize; i++){
+        if(nums[i] == 0)
+            sum--;
+        else   
+            sum++;
+
+        curCount = hash[sum + numsSize];
+
+        if(curCount != -2) {
+            if (res < i - curCount)
+                res = i - curCount;
+        } 
+        else {
+        hash[sum + numsSize] = i;
+        }
+    }
+    
+    free(hash);
+    return res;
+}
