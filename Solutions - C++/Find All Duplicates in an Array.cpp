4753ffe735024cfc22a5f8b180d0da1712768a52
emirgit <emirtr852816@hotmail.com>
2024-03-25 21:27:17 +0300

    Create Find All Duplicates in an Array.cpp

---
+++ Solutions - C++/Find All Duplicates in an Array.cpp
@@ -0,0 +1,25 @@
+/*
+Time C.
+32ms ms Beats 92.40%
+Space C.
+35.90 MB Beats 90.94%
+*/
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> res;
+        int numsSize = nums.size();
+        int index;
+
+        for (int i = 0; i < numsSize; i++) {
+            index = abs(nums[i]) - 1;
+            if (nums[index] < 0)
+                res.push_back(index + 1);
+            else
+                nums[index] = -nums[index]; 
+        }
+        return res;
+
+    }
+};
