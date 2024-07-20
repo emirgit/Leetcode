5f775d862ae852cb0aa31f0f512748424ed4fa5c
emirgit <emirtr852816@hotmail.com>
2024-07-20 23:33:20 +0300

    35
    Search Insert Position

---
+++ Solutions - Java/Search Insert Position.java
@@ -0,0 +1,29 @@
+/*
+Binary Search Algorithm
+
+Time C.
+0ms Beats 100.00%
+Space C.
+43.32 MB Beats 8.42%
+*/
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int r, l, mid;
+
+        l = 0;
+        r = nums.length - 1;
+        while(l <= r){
+            mid = l + (r - l) / 2;
+
+            if(nums[mid] < target)
+                l = mid + 1;
+            else if (nums[mid] > target)
+                r = mid - 1;
+            else
+                return mid;
+        }
+
+        return l;
+    }
+}
